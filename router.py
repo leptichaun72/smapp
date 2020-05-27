2cf5a4f073cc08d66dbaf732a2c7adb3288d5518
I am GAY AS FUCK <gster1234512345@mail.com>
2020-05-27 17:14:05 +0000

    Add test files

--- router.py
+++ router.py
@@ -2,8 +2,31 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.run()
-
 @app.route('/')
-def index():
-    return 'hello world'
+@app.route('/index/<book>')
+def index(book=None):
+    return 'My book is ' + book
+
+@app.route('/create/<first_name>/<last_name>')
+def create(first_name=None, last_name=None):
+  return 'Hello ' + first_name + ',' + last_name
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    print type(username)
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    print type(post_id)
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    print type(subpath)
+    # show the subpath after /path/
+    return 'Subpath %s' % subpath
+
+app.run(debug=True)
