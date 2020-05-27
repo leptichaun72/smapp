e07e767e9095cef01eff1ab087a41d495d7cff23
I am GAY AS FUCK <gster1234512345@mail.com>
2020-05-27 18:35:23 +0000

    Add working landing page for index

--- app.py
+++ app.py
@@ -7,9 +7,6 @@ def dashboard(name):
 
 @app.route('/')
 def index():
-    if 'username' in session:
-        username = session['username']
-        return 'Logged in as ' + username + '<br>' + "<b><a href = '/logout'>click here to log out</a></b>"
     return "You are not logged in <br><a href = '/login'>" + "click here to log in</a>"
 
 @app.route('/login',methods = ['POST', 'GET'])
