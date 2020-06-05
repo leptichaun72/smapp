2fa43079106e8a68696475bb3df2f82a2b517264
I am GAY AS FUCK <gster1234512345@mail.com>
2020-06-05 05:25:14 +0000

    Add greet path

---
+++ hello.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello home page!'
+
+@app.route('/greet', methods=['GET'])
+def greet():
+    name = request.args.get('name', 'Guest')
+    msg = f'Hello {name}'
+    
+    return msg, 200, {'Content-Type': 'text/plain; charset=utf-8'}
+
+
