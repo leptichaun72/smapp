5b6baba606c39ed801f44b2e1d5c3b5506e01d45
I am GAY AS FUCK <gster1234512345@mail.com>
2020-06-06 18:45:43 +0000

    Test make_resposnse function

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request
+from flask import Flask, request, make_response
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,7 +10,24 @@ def index():
 @app.route('/greet', methods=['GET'])
 def greet():
     name = request.args.get('name', 'Guest')
-    msg = f'Hello I am {name}'
+    msg = f'<h1>Hello I am {name}</h1>'
     
+    return msg, 201, {'Content-Type': 'text/html'}
+
+@app.route('/greet2/<name>/', methods=['POST'])
+def greet2(name):
+    msg = f'You are meeting {name}'
+
     return msg, 200, {'Content-Type': 'text/plain; charset=utf-8'}
 
+@app.route('/users/<name>', methods=['POST'])
+def create_user(name):
+
+    msg = f'user {name} created'
+    return make_response(msg, 201)
+
+@app.route('/users/<name>', methods=['GET'])
+def get_user(name):
+
+    msg = f'Hello {name}'
+    return make_response(msg, 200)
