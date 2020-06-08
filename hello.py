67d53e395e4760219b4bcd70231464a147eb214b
I am GAY AS FUCK <gster1234512345@mail.com>
2020-06-08 03:50:30 +0000

    Add make_response, send_file, templates, static, 404

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, send_file, jsonify, render_template
+import random
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,20 +16,52 @@ def greet():
     
     return msg, 201, {'Content-Type': 'text/html'}
 
-@app.route('/greet2/<name>/', methods=['POST'])
-def greet2(name):
-    msg = f'You are meeting {name}'
-
-    return msg, 200, {'Content-Type': 'text/plain; charset=utf-8'}
-
 @app.route('/users/<name>', methods=['POST'])
 def create_user(name):
 
     msg = f'user {name} created'
-    return make_response(msg, 201)
+    return make_response(msg, 205)
 
 @app.route('/users/<name>', methods=['GET'])
 def get_user(name):
 
     msg = f'Hello {name}'
     return make_response(msg, 200)
+
+@app.route('/image')
+def get_image():
+
+    filename = 'airplane-sunset.jpg'
+    return send_file(filename, mimetype='image/jpg')
+
+movies = {
+    1: 'Toy Story',
+    2: 'Star Wars',
+    3: 'Kung Fu Panda',
+    4: 'The Lion King',
+    5: 'Ip Man'
+}
+
+@app.route('/movies')
+def get_movies():
+    return movies
+
+# converting tuple to json through jsonify func
+@app.route('/movie/random')
+def random_movie():
+    movie = random.choice(list(movies.items()))
+    return jsonify(movie)
+
+@app.route('/stache/<animal>')
+def listen_animal(animal):
+    sound = f'Hear a {animal} roar!'
+    print(type(sound))
+    return render_template('animals/roar.html', creature=sound)
+
+@app.route('/about')
+def about():
+    return app.send_static_file('about.html')
+
+@app.errorhandler(404)
+def cant_find_page(err):
+    return render_template('404.html'), 404
