3070fb49f341f04b09752de834e8b8d066aa6e04
iamgayasfuck <leptichaun72@github.com>
2020-09-24 01:07:45 -0400

    Work in progress modeling

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from config import db_file 
 from views.index import primary_bp
 
@@ -6,6 +7,7 @@ application = Flask(__name__)
 application.config["SQLALCHEMY_DATABASE_URI"] = db_file
 application.register_blueprint(primary_bp)
 application.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
+db = SQLAlchemy(application)
 
 application.run(debug=True)
 
--- models.py
+++ models.py
@@ -1,2 +1,7 @@
-from flask_sqlalchemy import SQLAlchemy
+from app import db
 
+class Stock(db.Model):
+    id = db.Columm(db.Integer,primary_key=True)
+    name = db.Column(db.Text,unique=True)
+    symbol = db.Column(db.String(8),unique=True,nullable=False)
+    lots = db.relationship("Lot
